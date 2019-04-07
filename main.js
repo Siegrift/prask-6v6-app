85467e7d99bcd62b29f5d34ca87f84a82efa4360
Siegrift <e.tesarr@gmail.com>
2019-04-07 18:52:41 +0200

    Nearly finished

--- main.js
+++ main.js
@@ -1,7 +1,71 @@
 const startContainer = document.getElementById("start-container");
 const mainContainer = document.getElementById("main-container");
+const restartButton = document.getElementById("restart");
+const timeText = document.getElementById("time");
+const untappedCodeText = document.getElementById("untapped-code");
+const tappedCodeText = document.getElementById("tapped-code");
+const buttons = Array.apply(null, { length: 9 }).map((_, i) =>
+  document.getElementById(`btn${i + 1}`)
+);
+
+let startTime;
+let currentInterval;
+const updateTimeValue = () => {
+  timeText.innerHTML = (Date.now() - startTime) / 1000;
+};
+
+const LENGTH = 8;
+let untappedCode = "";
+let tappedCode = "";
+
+const updateCodePanel = () => {
+  untappedCodeText.innerHTML = untappedCode;
+  tappedCodeText.innerHTML = tappedCode;
+};
+
+const shuffleButtonText = () => {
+  // TODO: shuffle buttons
+};
+
+shuffleButtonText();
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (btn.innerHTML[0] === untappedCode[0]) {
+      tappedCode += untappedCode[0];
+      untappedCode = untappedCode.substr(1);
+      // TODO: check if untapped is empty and stop timer
+      shuffleButtonText();
+    } else {
+      untappedCode = tappedCode + untappedCode;
+      tappedCode = "";
+    }
+
+    updateCodePanel();
+  });
+});
 
 startContainer.addEventListener("click", () => {
   startContainer.classList.toggle("hidden");
   mainContainer.classList.toggle("hidden");
+
+  for (let i = 0; i < LENGTH; i++) {
+    untappedCode += Math.ceil(Math.random() * 9);
+  }
+  untappedCodeText.innerHTML = untappedCode;
+  updateCodePanel();
+
+  startTime = Date.now();
+  currentInterval = setInterval(updateTimeValue, 47);
+});
+
+restartButton.addEventListener("click", () => {
+  startContainer.classList.toggle("hidden");
+  mainContainer.classList.toggle("hidden");
+
+  untappedCode = "";
+  tappedCode = "";
+
+  clearInterval(currentInterval);
+  timeText.innerHTML = "";
 });
