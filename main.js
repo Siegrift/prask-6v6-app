15b0fb86641598617faf06512e2ffc341aad6558
Siegrift <e.tesarr@gmail.com>
2019-04-07 18:19:29 +0200

    Finished design

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('HELLO')
+const startContainer = document.getElementById("start-container");
+const mainContainer = document.getElementById("main-container");
+
+startContainer.addEventListener("click", () => {
+  startContainer.classList.toggle("hidden");
+  mainContainer.classList.toggle("hidden");
+});
